4eca0a9b929899f644722a489309751dce44184b
David <Rigby335@hotmail.com>
2022-05-09 11:37:27 -0700

    animal.js params

---
+++ animals/animals.js
@@ -0,0 +1,12 @@
+import { animals } from '../data.js';
+import { findById } from '../utils.js';
+
+const animalName = document.getElementById('animal-name');
+const animalImage = document.getElementById('animal-image');
+const animalSays = document.getElementById('animal-says');
+
+const params = new URLSearchParams(window.location.search);
+const animal = findById(params.get('id'), animals);
+animalName.textContent = animal.Name;
+animalImage.src = `../assets/${animal.type}.svg`;
+animalSays.textContent = animal.says;
